56d50e97c37f873579f3bc67991d24fd0ac0fe1b
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-12-01 16:55:35 +0100

    Added day01 solution

---
+++ day_01/solution.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+
+const zero = { int: '0', str: 'zero' };
+const one = { int: '1', str: 'one' };
+const two = { int: '2', str: 'two' };
+const three = { int: '3', str: 'three' };
+const four = { int: '4', str: 'four' };
+const five = { int: '5', str: 'five' };
+const six = { int: '6', str: 'six' };
+const seven = { int: '7', str: 'seven' };
+const eight = { int: '8', str: 'eight' };
+const nine = { int: '9', str: 'nine' };
+
+const returnFirstDigit = (str) => {
+  let index = 0;
+  let firstNum = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      firstNum += str[i];
+      index = i;
+      break;
+    }
+  }
+  const arr = [];
+  arr.push(
+    str.indexOf(zero.str),
+    str.indexOf(one.str),
+    str.indexOf(two.str),
+    str.indexOf(three.str),
+    str.indexOf(four.str),
+    str.indexOf(five.str),
+    str.indexOf(six.str),
+    str.indexOf(seven.str),
+    str.indexOf(eight.str),
+    str.indexOf(nine.str)
+  );
+  const newarr = arr.filter((item) => item !== -1);
+  const indexArr = Math.min(...newarr);
+  if (indexArr < index) {
+    if (str[indexArr] == 'z') return zero.int.toString();
+    else if (str[indexArr] == 'o') return one.int.toString();
+    else if (str[indexArr] == 't' && str[indexArr + 1] == 'w')
+      return two.int
+    else if (str[indexArr] == 't' && str[indexArr + 1] == 'h')
+      return three.int
+    else if (str[indexArr] == 'f' && str[indexArr + 1] == 'o')
+      return four.int
+    else if (str[indexArr] == 'f' && str[indexArr + 1] == 'i')
+      return five.int
+    else if (str[indexArr] == 's' && str[indexArr + 1] == 'i')
+      return six.int
+    else if (str[indexArr] == 's' && str[indexArr + 1] == 'e')
+      return seven.int
+    else if (str[indexArr] == 'e') return eight.int
+    else if (str[indexArr] == 'n') return nine.int
+  }
+  return firstNum;
+};
+
+const returnLastDigit = (str) => {
+  let index = 0;
+  let secondNum = '';
+  for (let i = str.length; i >= 0; i--) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      secondNum += str[i];
+      index = i;
+      break;
+    }
+  }
+  const arr = [];
+  arr.push(
+    str.lastIndexOf(zero.str),
+    str.lastIndexOf(one.str),
+    str.lastIndexOf(two.str),
+    str.lastIndexOf(three.str),
+    str.lastIndexOf(four.str),
+    str.lastIndexOf(five.str),
+    str.lastIndexOf(six.str),
+    str.lastIndexOf(seven.str),
+    str.lastIndexOf(eight.str),
+    str.lastIndexOf(nine.str)
+  );
+  const newarr = arr.filter((item) => item !== -1);
+  const indexArr = Math.max(...newarr);
+  if (indexArr > index) {
+    if (str[indexArr] == 'z') return zero.int
+    else if (str[indexArr] == 'o') return one.int
+    else if (str[indexArr] == 't' && str[indexArr + 1] == 'w')
+      return two.int
+    else if (str[indexArr] == 't' && str[indexArr + 1] == 'h')
+      return three.int
+    else if (str[indexArr] == 'f' && str[indexArr + 1] == 'o')
+      return four.int
+    else if (str[indexArr] == 'f' && str[indexArr + 1] == 'i')
+      return five.int
+    else if (str[indexArr] == 's' && str[indexArr + 1] == 'i')
+      return six.int
+    else if (str[indexArr] == 's' && str[indexArr + 1] == 'e')
+      return seven.int
+    else if (str[indexArr] == 'e') return eight.int
+    else if (str[indexArr] == 'n') return nine.int
+  }
+  return secondNum;
+};
+
+fs.readFile('data.txt', (err, inputD) => {
+  if (err) throw err;
+  let array = inputD
+    .toString()
+    .split('\n')
+    .map((str) => {
+      let arr = [];
+      let result = [];
+      const firstNum = returnFirstDigit(str);
+      const secondNum = returnLastDigit(str);
+      arr.push(firstNum);
+      arr.push(secondNum);
+      const num = parseInt(arr[0] + arr[1]);
+      result.push(num);
+      return result.reduce((a, b) => a + b, 0);
+    });
+  console.log(array.reduce((a, b) => a + b, 0));
+});
