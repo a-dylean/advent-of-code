7c3eb657a0cbfdc09792963dce1df40b13626317
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-12-02 18:26:47 +0100

    Part 2 solved

---
+++ day_02/solution-part2.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const calculateResult = (games) => {
+  const arrOfPowers = [];
+  for (game of games) {
+    const power = game.blue * game.red * game.green;
+    arrOfPowers.push(power);
+  }
+  return arrOfPowers.reduce((a, b) => a + b, 0);
+};
+const biggestColorValues = (colors) => {
+  const colorValues = {};
+
+  colors.forEach((color) => {
+    const [value, colorName] = color.split(" ");
+    const numericValue = parseInt(value);
+    if (!(colorName in colorValues) || numericValue > colorValues[colorName]) {
+      colorValues[colorName] = numericValue;
+    }
+  });
+
+  return colorValues;
+};
+
+fs.readFile("data.txt", (err, inputD) => {
+  if (err) throw err;
+  let array = inputD
+    .toString()
+    .split("\n")
+    .map((gameString) => {
+      const [gameNum, ...rounds] = gameString.split(": ");
+      return {
+        [gameNum]: rounds
+          .toString()
+          .split(/,|;/)
+          .map((x) => x.trim()),
+      };
+    });
+  const biggestNums = [];
+  for (let i = 0; i < array.length; i++) {
+    Object.values(array[i]).forEach((rounds) => {
+      biggestNums.push(biggestColorValues(rounds));
+    });
+  }
+  console.log(calculateResult(biggestNums));
+});
