e2a56aa9a4659dd237df281c786507d46118e8da
Alice Tonkopiy <adtonkopiy@gmail.com>
2024-12-04 18:13:53 +0100

    added day4 part2

--- 2024/day_04/solution-part2.js
+++ 2024/day_04/solution-part2.js
@@ -1,19 +1,20 @@
-import * as fs from 'fs';
-const data: string = fs.readFileSync('./data', 'utf-8');
-
-const rows : string[] = data.split('\n');
-const map : any[] = [];
-let count: number = 0;
-for (let row of rows) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var data = fs.readFileSync('./data', 'utf-8');
+var rows = data.split('\n');
+var map = [];
+var count = 0;
+for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
+    var row = rows_1[_i];
     map.push(row.split(''));
 }
-
-for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
+for (var y = 0; y < map.length; y++) {
+    for (var x = 0; x < map[y].length; x++) {
         if (map[y][x] == 'A') {
-            if (map[y + 1] && map[y - 1] && map[y + 1][x + 1] && map[y + 1][x - 1] && map[y - 1][x + 1] && map[y - 1][x - 1] && map[y + 1][x + 1] == 'S' && map[y + 1][x - 1] == 'M' && map[y - 1][x + 1] == 'S' && map[y - 1][x - 1] == 'M') { 
+            if (map[y + 1] && map[y - 1] && map[y + 1][x + 1] && map[y + 1][x - 1] && map[y - 1][x + 1] && map[y - 1][x - 1] && map[y + 1][x + 1] == 'S' && map[y + 1][x - 1] == 'M' && map[y - 1][x + 1] == 'S' && map[y - 1][x - 1] == 'M') {
                 count++;
-            } 
+            }
             if (map[y + 1] && map[y - 1] && map[y + 1][x + 1] && map[y + 1][x - 1] && map[y - 1][x + 1] && map[y - 1][x - 1] && map[y + 1][x + 1] == 'M' && map[y + 1][x - 1] == 'M' && map[y - 1][x + 1] == 'S' && map[y - 1][x - 1] == 'S') {
                 count++;
             }
@@ -26,5 +27,4 @@ for (let y = 0; y < map.length; y++) {
         }
     }
 }
-
 console.log(count);
